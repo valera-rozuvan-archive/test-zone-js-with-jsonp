e77c8dfc952801b4b02f3df1d4c2cf14d6664846
Vieno Jyoti <Valerii.Rozuvan@excelian.com>
2018-01-23 18:55:15 +0200

    Initial implementation

--- README.md
+++ README.md
@@ -1,2 +1,78 @@
-# test-zone-js-with-jsonp
-Test how Zone.js handles JSONP
+# Test how Zone.js handles JSONP
+
+This repository tries to confirm the fact that [Zone.js](https://github.com/angular/zone.js/) can't
+(doesn't) monkey patch JSONP. The project consists of a front-end application, and a back-end application.
+In the web app interface, there are two buttons. The first button triggers a GET request, the second
+button triggers a JSONP request. The entire application is wrapped inside a Zone, and we output a
+bunch of logs to the JavaScript console. Also, we perform a console.log() directly from within each of
+the response handlers.
+
+These are the JS console logs from GET request:
+
+```
+new zone: onInvokeTask
+eventName = click
+eventName = HTMLButtonElement.addEventListener:click
+new zone: onInvokeTask
+eventName = click
+eventName = HTMLDocument.addEventListener:click
+new zone: onScheduleTask
+eventName = undefined
+eventName = XMLHttpRequest.addEventListener:load
+new zone: onScheduleTask
+eventName = undefined
+eventName = XMLHttpRequest.send
+new zone: onHasTask
+new zone: onInvokeTask
+---> eventName = undefined
+---> eventName = XMLHttpRequest.send
+new zone: onHasTask
+new zone: onInvokeTask
+---> eventName = load
+---> eventName = XMLHttpRequest.addEventListener:load
+app.js:65 GET request OK. Data is  {"data":"[get ok 42]"}
+```
+
+These are the JS console logs from JSONP request:
+
+```
+new zone: onInvokeTask
+---> eventName = click
+---> eventName = HTMLButtonElement.addEventListener:click
+new zone: onInvokeTask
+---> eventName = click
+---> eventName = HTMLDocument.addEventListener:click
+new zone: onScheduleTask
+---> eventName = undefined
+---> eventName = setTimeout
+new zone: onHasTask
+new zone: onHasTask
+JSONP request OK. Data is  {"data":"[jsonp ok 97]"}
+```
+
+As can be seen, Zone.js does not catch the JSONP response. This is because
+the response is actually JavaScript code that is executed by the browser inside
+a newly injected `script` tag.
+
+Further reading: [Do you still think that NgZone (zone.js) is required for change detection in Angular?](https://blog.angularindepth.com/do-you-still-think-that-ngzone-zone-js-is-required-for-change-detection-in-angular-16f7a575afef)
+
+## Run locally
+
+Make sure you have [node](https://github.com/nodejs/node), [npm](https://github.com/npm/npm), and
+[nodemon](https://github.com/remy/nodemon) installed globally.
+
+1. Clone this repository.
+2. Switch to repository root folder.
+3. Run command `npm install`.
+4. Run command `npm run start`.
+5. Navigate your browser to `http://localhost:3000/`.
+
+## Local development
+
+For convenience of local development, Nodemon is used for watching server JS files, and restarting the server upon
+changes. [LiveReload](https://github.com/napcs/node-livereload) is integrated into the front-end part to refresh the
+browser when the web application source files change.
+
+## License
+
+This project is licensed under the MIT License. See [LICENSE](LICENSE) for more details.
---
+++ web_app/js/app.js
@@ -0,0 +1,245 @@
+const noop = () => { };
+
+// Borrowed from:
+//   https://codepad.co/snippet/v3nM2Oj2
+const getJSONP = (() => {
+    return {
+        send: (src, options) => {
+            let callback_name = options.callbackName || 'foo',
+                on_success = options.onSuccess || noop,
+                on_timeout = options.onTimeout || noop,
+                timeout = options.timeout || 10; // seconds
+
+            let timeout_trigger = window.setTimeout(() => {
+                window[callback_name] = noop;
+                on_timeout();
+            }, timeout * 1000);
+
+            window[callback_name] = (data) => {
+                window.clearTimeout(timeout_trigger);
+                on_success(data);
+            }
+
+            let script = document.createElement('script');
+            script.type = 'text/javascript';
+            script.async = true;
+            script.src = src;
+
+            document.getElementsByTagName('head')[0].appendChild(script);
+        }
+    };
+})();
+
+const makeJsonpRequest = (store) => {
+    getJSONP.send('/api/simple_jsnop?callback=execute_jsonp_callback', {
+        callbackName: 'execute_jsonp_callback',
+        onSuccess: (responseText) => {
+            console.log('JSONP request OK. Data is ', responseText);
+
+            let response = {
+                data: ''
+            };
+
+            try {
+                response = JSON.parse(responseText);
+            } catch (err) {
+                response = {
+                    data: 'invalid JSON'
+                };
+            }
+
+            store.dispatch({
+                type: 'JSONP_COMPLETE',
+                data: response.data
+            });
+        }
+    });
+};
+
+const makeGetRequest = (store) => {
+    let xhr = new XMLHttpRequest();
+
+    xhr.open('GET', 'api/simple_get');
+    xhr.onload = () => {
+        if (xhr.status === 200) {
+            console.log('GET request OK. Data is ', xhr.responseText);
+
+            let response = {
+                data: ''
+            };
+
+            try {
+                response = JSON.parse(xhr.responseText);
+            } catch (err) {
+                response = {
+                    data: 'invalid JSON'
+                };
+            }
+
+            store.dispatch({
+                type: 'GET_COMPLETE',
+                data: response.data
+            });
+        } else {
+            console.log('GET request failed. Returned status of ', xhr.status);
+        }
+    };
+    xhr.send();
+};
+
+const reducer = (state, action) => {
+    if (typeof state === 'undefined') {
+        return {
+            getTriggered: false,
+            getResult: '',
+
+            jsonpTriggered: false,
+            jsonpResult: ''
+        };
+    }
+
+    let newState = {
+        getTriggered: state.getTriggered,
+        getResult: state.getResult,
+
+        jsonpTriggered: state.jsonpTriggered,
+        jsonpResult: state.jsonpResult
+    };
+
+    switch (action.type) {
+        case 'TRIGGER_GET':
+            newState.getTriggered = true;
+            makeGetRequest(store);
+            break;
+        case 'TRIGGER_JSONP':
+            newState.jsonpTriggered = true;
+            makeJsonpRequest(store);
+            break;
+        case 'GET_COMPLETE':
+            newState.getResult = action.data;
+            break;
+        case 'JSONP_COMPLETE':
+            newState.jsonpResult = action.data;
+            break;
+        default:
+            break;
+    }
+
+    return newState;
+};
+
+const { createStore } = Redux;
+const store = createStore(reducer);
+
+const ResultsComponent = ({
+    getTriggered, getResult,
+    jsonpTriggered, jsonpResult
+}) => (
+        <div>
+            {
+                (() => {
+                    if (getTriggered) {
+                        return <div>GET result: {getResult}</div>;
+                    }
+                })()
+            }
+
+            {
+                (() => {
+                    if (jsonpTriggered) {
+                        return <div>JSONP result: {jsonpResult}</div>;
+                    }
+                })()
+            }
+        </div>
+    );
+
+const ButtonsComponent = ({ triggerGet, triggerJsonp }) => (
+    <div>
+        <button onClick={triggerGet}>Get</button>
+        <button onClick={triggerJsonp}>JSONP</button>
+    </div>
+);
+
+class RootComponent extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const currentState = store.getState();
+
+        return <div>
+            <ResultsComponent
+                getTriggered={currentState.getTriggered}
+                getResult={currentState.getResult}
+                jsonpTriggered={currentState.jsonpTriggered}
+                jsonpResult={currentState.jsonpResult}
+            />
+            <ButtonsComponent
+                triggerGet={() => {
+                    store.dispatch({
+                        type: 'TRIGGER_GET'
+                    });
+                }}
+                triggerJsonp={() => {
+                    store.dispatch({
+                        type: 'TRIGGER_JSONP'
+                    });
+                }}
+            />
+        </div>;
+    }
+}
+
+const render = () => {
+    ReactDOM.render(
+        <RootComponent />,
+        document.getElementById('root')
+    );
+};
+
+const rootZone = Zone.current;
+const newZone = rootZone.fork({
+    name: 'new',
+    onScheduleTask: (delegate, currentZone, targetZone, task) => {
+        console.log('new zone: onScheduleTask');
+        console.log(`---> eventName = ${task.eventName}`);
+        console.log(`---> eventName = ${task.source}`);
+
+        return delegate.scheduleTask(targetZone, task);
+    },
+    onInvokeTask: (delegate, current, target, task, applyThis, applyArgs) => {
+        console.log('new zone: onInvokeTask');
+        console.log(`---> eventName = ${task.eventName}`);
+        console.log(`---> eventName = ${task.source}`);
+
+        try {
+            return delegate.invokeTask(target, task, applyThis, applyArgs);
+        } finally {
+
+        }
+    },
+    cancelTask: () => {
+        console.log('new zone: cancelTask');
+    },
+    onHasTask: () => {
+        console.log('new zone: onHasTask');
+    },
+    onHandleError: (delegate, current, target, error) => {
+        console.log('new zone: onHandleError');
+        console.log(error);
+
+        delegate.handleError(target, error);
+
+        return false;
+    }
+});
+console.log(rootZone.name, newZone.parent.name);
+
+newZone.run(() => {
+    console.log(Zone.current.name, Zone.current === newZone);
+
+    store.subscribe(render);
+    render();
+});
